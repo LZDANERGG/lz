9a42b9bae9c526ab305e45d64b96e7fcdf4c775f
Lzdaner <lzdaner@163.com>
2020-04-08 14:58:45 +0800

    update

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
-import VueLazyLoad from 'vue-lazyload'
+import store from './store';
+import VueLazyLoad from 'vue-lazyload';
 
 Vue.config.productionTip = false
 
